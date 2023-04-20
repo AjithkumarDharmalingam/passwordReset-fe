1d6ce7d355c2fe034806f00976cee0d86e7dbbc7
AjithkumarDharmalingam <ajithkumarbe97@gmail.com>
2023-04-20 20:51:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Forgotpassword from './components/Forgotpassword';
+import Passwordreset from './components/Passwordreset';
+import Dashboard from './components/Dashboard';
+
+
+export const url = 'https://passwordreset-wqsq.onrender.com'
+
+function App() {
+  return<>
+   <BrowserRouter>
+    <Routes>
+      <Route path='login' element={<Login/>}/>
+      <Route path='signup' element={<Signup/>}/>
+      <Route path='dashboard' element={<Dashboard/>}/>
+      <Route path='passwordreset' element={<Passwordreset/>}/>
+      <Route path='/forgotpassword/:id/:token' element={<Forgotpassword/>}/>
+      <Route path="*" element={<Navigate to="/login" />} />
+    </Routes>
+   </BrowserRouter>
+  </>
+}
+
+export default App;
